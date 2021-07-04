63374730b82e660bf49517ee44c0d9fef75bf4c6
Mayank Sharma <=>
2021-07-04 13:36:28 +0530

    Init + Added Home Page.

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './home.css';
+import avatar1 from '../assets/avatar01.png';
+import avatar2 from '../assets/avatar02.png';
+
+function Home(props) {
+  return (
+    <div className="login-wrapper">
+      <div className="user-wrapper-1">
+        <div className="user-icon-1">
+          <img src={avatar1} alt="user-avatar-1"/>
+        </div>
+        <div className="user-details">
+          Player 01
+          <input type="text" className="user-name"/>
+        </div>
+      </div>
+      <div className="user-wrapper-2">
+        <div className="user-icon-2">
+          <img src={avatar2} alt="user-avatar-2"/>
+        </div>
+        <div className="user-details">
+          Player 02
+          <input type="text" className="user-name"/>
+        </div>
+      </div>
+      <span className="separator" />
+      <button className="start-button">
+        Start Game
+      </button>
+    </div>
+  );
+}
+
+export default Home;
